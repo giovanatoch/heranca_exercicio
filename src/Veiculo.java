84277264c19d58decbb648e10e7968340bf071ba
giovanatoch <tochtrop.giovana@gmail.com>
2024-03-26 20:12:53 -0300

    add os Set na classe Veiculo

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -10,19 +10,35 @@ class Veiculo {
         this.ano = ano;
     }
 
+    //getMarca
     public String getMarca() {
         return marca;
     }
+    public void setMarca(String marca) {
+         this.marca = marca;
+    }
 
+    //getModelo
     public String getModelo() {
         return modelo;
     }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
+    //getCor
     public String getCor() {
         return cor;
     }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 
+    //getAno
     public int getAno() {
         return ano;
+    }
+    public void getAno(int ano) {
+        this.ano = ano;
     }
 }
